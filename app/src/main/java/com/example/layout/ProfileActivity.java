0c301d8330edcfbc606c472ab52be81568b39f34
willz0207 <william.robert@ti.ukdw.ac.id>
2020-03-06 02:24:30 +0700

    push button

---
+++ app/src/main/java/com/example/layout/ProfileActivity.java
@@ -0,0 +1,12 @@
+package com.example.layout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate();
+    }
+}
