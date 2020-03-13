fb22dd55fc398e8f128ebc3f98d53e7c5c844c77
willz0207 <61018506+willz0207@users.noreply.github.com>
2020-03-13 16:50:31 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/layout/MainActivity.java
+++ app/src/main/java/com/example/layout/MainActivity.java
@@ -1,42 +1,63 @@
-package com.example.layout;
+ package com.example.layout;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.content.SharedPreferences;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.Toast;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+    import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
+        private Button loginBtn;
+        private EditText emailTxt;
+        private EditText passTxt;
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        public static final String myPREFERENCES = "MyPrefs";
+        public static final String Email = "emailKey";
+        SharedPreferences sharedPreferences;
 
-    EditText email;
-    EditText passwd;
-    Button button;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        email = findViewById(R.id.emailtxt);
-        passwd = findViewById(R.id.passwordtxt);
-        button = findViewById(R.id.loginBtn);
+            emailTxt = findViewById(R.id.emailTxt);
+            passTxt = findViewById(R.id.passTxt);
+            loginBtn = (Button) findViewById(R.id.loginBtn);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "Selamat Datang Gayn", Toast.LENGTH_SHORT).show();
-                openActivity2();
-            }
-        });
+            sharedPreferences = getSharedPreferences(myPREFERENCES, Context.MODE_PRIVATE);
 
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (emailTxt.getText().toString().trim().matches(emailPattern)){
+                        if (passTxt.getText().toString().isEmpty()){
+                            Toast.makeText(getApplicationContext(),"Password is empty !",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString(Email, emailTxt.getText().toString());
+                            editor.commit();
+                            openArctivityHome();
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Incorrect email and password !",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
+        public void openArctivityHome(){
+            Bundle bundle = new Bundle();
+            bundle.putString("dataUsername", emailTxt.getText().toString());
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
     }
-    public void openActivity2(){
-        Intent intent = new Intent( this, MainActivity2.class);
-        startActivity(intent);
 
-    }
-}
